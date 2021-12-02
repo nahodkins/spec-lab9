19f6431189b34a687e7ce580b8300f09eb17c2dc
nahodkins <tsuber2001@gmail.com>
2021-12-02 22:23:54 +0200

    Adds second task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static List<String> readFile(String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            return reader.lines()
+                    .map(str -> str.split(" "))
+                    .flatMap(Arrays::stream)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> first = readFile("first.txt");
+        List<String> second = readFile("second.txt");
+        System.out.println("First: " + first);
+        System.out.println("Second: " + second);
+
+        Set<String> union = new HashSet<>(first);
+        union.addAll(second);
+        System.out.println("Union: " + union);
+
+        Set<String> intersection = first.stream()
+                .distinct()
+                .filter(second::contains)
+                .collect(Collectors.toSet());
+        System.out.println("Intersection: " + intersection);
+
+        Set<String> difference = new HashSet<>(first);
+        second.forEach(difference::remove);
+        System.out.println(difference);
     }
 }

df2367d7a3ffe1ff5196ed6665a45f338253bc44
nahodkins <tsuber2001@gmail.com>
2021-12-02 22:10:40 +0200

    Adds first task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
 
+    private static void printSetUsingIterator(Set<String> strings) {
+        Iterator<String> iterator = strings.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    private static void printSetUsingForEach(Set<String> strings) {
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+
+    private static void printSetUsingToString(Set<String> strings) {
+        System.out.println(strings);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (BufferedReader reader = new BufferedReader(new FileReader("file.txt"))){
+            Set<String> strings = new HashSet<>();
+            reader.lines()
+                    .map(str -> str.split(" "))
+                    .flatMap(Arrays::stream)
+                    .forEach(strings::add);
+
+            printSetUsingIterator(strings);
+            System.out.println();
+            printSetUsingForEach(strings);
+            System.out.println();
+            printSetUsingToString(strings);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
